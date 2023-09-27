1a98aa81fd3e87b145cb33301b7cb717fcd246b4
mthierman <mthierman@gmail.com>
2023-09-26 21:30:25 -0600

    Update button style

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 export default function App() {
     return (
-        <button className="m-4 rounded border-4 border-neutral-400 p-4 text-neutral-400">
+        <button className="rounded border-2 p-2">
             I'm a button
         </button>
     );
